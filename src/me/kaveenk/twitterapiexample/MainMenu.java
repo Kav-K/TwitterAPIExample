5fab0c4375ec15738bd904cfe9c05dcc34e9dd5f
Kaveen Kumarasinghe <ocfinancesmc@gmail.com>
2018-12-04 23:30:08 -0500

    Made MainMenu look nicer

--- src/me/kaveenk/twitterapiexample/MainMenu.java
+++ src/me/kaveenk/twitterapiexample/MainMenu.java
@@ -5,13 +5,24 @@
  */
 package me.kaveenk.twitterapiexample;
 
+import java.awt.Color;
+import java.awt.Point;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.util.stream.Collectors;
+import javax.imageio.ImageIO;
 import javax.swing.DefaultListModel;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JList;
 import javax.swing.JOptionPane;
@@ -27,26 +38,92 @@ import twitter4j.conf.ConfigurationBuilder;
  * @author kaveen
  */
 public class MainMenu extends javax.swing.JFrame {
-
+    
     private static Twitter twitter;
     private static ArrayList<String> staticTweetList;
+    private static ArrayList<String> staticLinkList;
     private static String name;
- 
+    static Point mouseDownCompCoords;
 
     /**
      * Creates new form MainMenu
      */
     public MainMenu() {
-        
+        this.setUndecorated(true);
         initComponents();
+        setBackgroundLabel();
+        initMouseListener();
+        
         this.setLocationRelativeTo(null);
         this.setResizable(false);
+        
+        
+        stylizeButtons();
         initialize();
-
+        
     }
-
+    private void stylizeButtons() {
+        exitButton.setBackground(new Color(50,204,254));
+        minimizeButton.setBackground(new Color(50,204,254));
+        searchButton.setBackground(new Color(50,204,254));
+        tweetsList.setBackground(new Color(128,223,255));
+      
+        
+    }
+    
+    private void initMouseListener() {
+        try {
+            this.addMouseListener(new MouseListener() {
+                public void mouseReleased(MouseEvent e) {
+                    mouseDownCompCoords = null;
+                }
+                
+                public void mousePressed(MouseEvent e) {
+                    mouseDownCompCoords = e.getPoint();
+                }
+                
+                public void mouseExited(MouseEvent e) {
+                }
+                
+                public void mouseEntered(MouseEvent e) {
+                }
+                
+                public void mouseClicked(MouseEvent e) {
+                }
+            });
+            
+            this.addMouseMotionListener(new MouseMotionListener() {
+                public void mouseMoved(MouseEvent e) {
+                }
+                
+                public void mouseDragged(MouseEvent e) {
+                    Point currCoords = e.getLocationOnScreen();
+                    setLocation(currCoords.x - mouseDownCompCoords.x, currCoords.y - mouseDownCompCoords.y);
+                }
+            });
+        } catch (Exception e) {
+            //Silence
+        }
+        
+    }
+    
+    private void setBackgroundLabel() {
+        BufferedImage background = null;
+        
+        try {
+            background = ImageIO.read(new File("resources/bg.jpg"));
+            
+        } catch (Exception e) {
+            //Silence, for now
+        }
+        JLabel backgroundLabel = new JLabel(new ImageIcon(background));
+        backgroundLabel.setBounds(0, 0, backgroundLabel.getPreferredSize().width, backgroundLabel.getPreferredSize().height);
+        
+        this.getContentPane().add(backgroundLabel);
+    }
+    
     private void initialize() {
-
+        
         ConfigurationBuilder cb = new ConfigurationBuilder();
         cb.setDebugEnabled(true)
                 .setOAuthConsumerKey("71Kw7SX1GwEelZH5Emg474Z6h")
@@ -55,13 +132,14 @@ public class MainMenu extends javax.swing.JFrame {
                 .setOAuthAccessTokenSecret("kyEn7NRXE6Q8U0OHMsqv7pEKkc6iRYF27uVhfelV1Emw2").setTweetModeExtended(true);
         TwitterFactory tf = new TwitterFactory(cb.build());
         twitter = tf.getInstance();
-
+        
     }
-
+    
     public List<String> getTweets(String user) {
-        ArrayList<String> timelineList = new ArrayList<String>();
+        ArrayList timelineList = new ArrayList<String>();
         try {
-            twitter.getUserTimeline(user, new Paging(1,(int) resultsAmountSpinner.getValue())).stream().forEach(item -> {
+            twitter.getUserTimeline(user, new Paging(1, (int) resultsAmountSpinner.getValue())).stream().forEach(item -> {
+                
                 if (item.isRetweet()) {
                     timelineList.add("(" + item.getCreatedAt().toString() + ") " + item.getRetweetedStatus().getText());
                 } else {
@@ -70,7 +148,7 @@ public class MainMenu extends javax.swing.JFrame {
             });
             staticTweetList = timelineList;
             return timelineList;
-
+            
         } catch (TwitterException ex) {
             ex.printStackTrace();
             return Arrays.asList("Error occurred. Check if the user is valid");
@@ -93,6 +171,8 @@ public class MainMenu extends javax.swing.JFrame {
         jLabel1 = new javax.swing.JLabel();
         resultsAmountSpinner = new javax.swing.JSpinner();
         jLabel2 = new javax.swing.JLabel();
+        minimizeButton = new javax.swing.JButton();
+        exitButton = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -115,7 +195,23 @@ public class MainMenu extends javax.swing.JFrame {
 
         resultsAmountSpinner.setModel(new javax.swing.SpinnerNumberModel(20, 1, 2000, 1));
 
+        jLabel2.setFont(new java.awt.Font("Ubuntu", 1, 14)); // NOI18N
         jLabel2.setText("Max Results:");
+
+        minimizeButton.setText("-");
+        minimizeButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                minimizeButtonActionPerformed(evt);
+            }
+        });
+
+        exitButton.setFont(new java.awt.Font("URW Gothic L", 0, 15)); // NOI18N
+        exitButton.setText("x");
+        exitButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                exitButtonActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -126,24 +222,32 @@ public class MainMenu extends javax.swing.JFrame {
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addComponent(tweetScrollPane)
                     .addGroup(layout.createSequentialGroup()
-                        .addGap(48, 48, 48)
-                        .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 364, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addGap(0, 0, Short.MAX_VALUE))
-                    .addGroup(layout.createSequentialGroup()
                         .addComponent(searchField, javax.swing.GroupLayout.PREFERRED_SIZE, 163, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                         .addComponent(searchButton)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 27, Short.MAX_VALUE)
                         .addComponent(jLabel2)
-                        .addGap(3, 3, 3)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                         .addComponent(resultsAmountSpinner, javax.swing.GroupLayout.PREFERRED_SIZE, 78, javax.swing.GroupLayout.PREFERRED_SIZE)))
                 .addContainerGap())
+            .addGroup(layout.createSequentialGroup()
+                .addGap(0, 0, Short.MAX_VALUE)
+                .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 364, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(minimizeButton, javax.swing.GroupLayout.PREFERRED_SIZE, 34, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(2, 2, 2)
+                .addComponent(exitButton, javax.swing.GroupLayout.PREFERRED_SIZE, 38, javax.swing.GroupLayout.PREFERRED_SIZE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addContainerGap()
-                .addComponent(jLabel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
+                            .addComponent(exitButton, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                            .addComponent(minimizeButton, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                        .addGap(0, 16, Short.MAX_VALUE))
+                    .addComponent(jLabel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(searchField, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
@@ -151,7 +255,7 @@ public class MainMenu extends javax.swing.JFrame {
                     .addComponent(resultsAmountSpinner, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(jLabel2))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(tweetScrollPane, javax.swing.GroupLayout.DEFAULT_SIZE, 390, Short.MAX_VALUE)
+                .addComponent(tweetScrollPane, javax.swing.GroupLayout.DEFAULT_SIZE, 396, Short.MAX_VALUE)
                 .addContainerGap())
         );
 
@@ -160,14 +264,14 @@ public class MainMenu extends javax.swing.JFrame {
 
     private void searchButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_searchButtonActionPerformed
         DefaultListModel model = new DefaultListModel();
-
+        
         for (String s : getTweets(searchField.getText())) {
             model.addElement(s);
         }
         this.name = "@" + searchField.getText();
-
+        
         tweetsList.setModel(model);
-
+        
 
     }//GEN-LAST:event_searchButtonActionPerformed
 
@@ -175,12 +279,21 @@ public class MainMenu extends javax.swing.JFrame {
         if (evt.getClickCount() == 2) {
             JList source = (JList) evt.getSource();
             int index = source.getSelectedIndex();
-
+            
             new SingularDisplayPane(name, staticTweetList.get(index)).setVisible(true);
-
+            
         }
 
     }//GEN-LAST:event_tweetsListMouseClicked
+
+    private void minimizeButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_minimizeButtonActionPerformed
+        this.setState(JFrame.ICONIFIED);
+    }//GEN-LAST:event_minimizeButtonActionPerformed
+
+    private void exitButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_exitButtonActionPerformed
+        this.dispose();
+        System.exit(0);
+    }//GEN-LAST:event_exitButtonActionPerformed
 
     /**
      * @param args the command line arguments
@@ -218,8 +331,10 @@ public class MainMenu extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton exitButton;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
+    private javax.swing.JButton minimizeButton;
     private javax.swing.JSpinner resultsAmountSpinner;
     private javax.swing.JButton searchButton;
     private javax.swing.JTextField searchField;
